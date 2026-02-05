f97f7aaa85f179fc385641c5e91f7edc4b19c5e2
DLizanaDev <Helgait@usal.es>
2026-02-05 12:54:08 +0100

    Solved exercise implementing Runnable and creating different threads to run each equation

--- Reto2/Reto2Hilos.java
+++ Reto2/Reto2Hilos.java
@@ -1,19 +1,34 @@
 package Reto2;
 
 public class Reto2Hilos {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         Sumatorio s1 = new Sumatorio(20);
         Sumatorio s2 = new Sumatorio(23);
         Sumatorio s3 = new Sumatorio(12);
 
-        // 2. Arrancar los hilos (esto inicia el metodo run() en paralelo)
-        s1.start();
-        s2.start();
-        s3.start();
+        Thread t1 = new Thread(s1);
+        Thread t2 = new Thread(s2);
+        Thread t3 = new Thread(s3);
+
+        // Arrancar los hilos (esto inicia el metodo run() en paralelo)
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try{
+            t1.join();
+            t1.join();
+            t1.join();
+
+            double formula = (s1.getResultado() + s2.getResultado() + s3.getResultado())/8;
+
+            System.out.println("El resultado es: "+formula);
 
 
-
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 }
