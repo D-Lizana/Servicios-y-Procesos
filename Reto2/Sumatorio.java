595a8b300c78cab31aa33b16bbd33b1baac0b38b
DLizanaDev <Helgait@usal.es>
2026-02-05 12:17:39 +0100

    Created class extending from Thread to override run() method

--- Reto2/Sumatorio.java
+++ Reto2/Sumatorio.java
@@ -1,4 +1,30 @@
 package Reto2;
 
-public class Sumatorio {
+public class Sumatorio extends Thread{
+    private int numero;
+    private double resultado;
+
+    public Sumatorio(int numero){
+        this.numero = numero;
+    }
+
+    @Override
+    public void run(){
+        if (numero % 2 == 0) {
+            numero = numero - 1;
+        }
+
+        double total = 0;
+        for (int i = numero; i >= 1; i = i - 2) {
+            total += i;
+        }
+        this.resultado = total;
+    }
+
+    public double getResultado(){
+        return  resultado;
+    }
+
+
+
 }
